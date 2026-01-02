5045a6017e4d5e8414905048fc2b7ea229fcc7fa
Chijindu-dev <chijinduenyinnaya29@gmail.com>
2026-01-02 12:11:38 -0800

    Clean up project: remove test scripts and move solc to devDependencies

--- scripts/test_run.js
+++
@@ -1,7 +0,0 @@
-console.log("Hardhat is working!");
-const hre = require("hardhat");
-console.log("HRE loaded.");
-async function main() {
-  console.log("Inside main, network:", hre.network.name);
-}
-main();
